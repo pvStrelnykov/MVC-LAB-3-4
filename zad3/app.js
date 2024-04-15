c81727656920fc686a1316149895cbabc8f61a57
StrelnikovPavlo <strelnikovpavel82@gmail.com>
2024-04-15 22:32:29 +0200

    zadanie 3

---
+++ zad3/app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import { getAddNewStudentPage, addStudent } from './controllers/students.js'
+import { getNotFoundPage } from './controllers/error.js'
+
+const __dirname = path.resolve()
+const PORT = 3000
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'zad3', 'views'))
+
+app.get('/', (req, res) => {
+	res.render('Home', { pageTitle: 'Home' })
+})
+app.get('/success', (req, res) => {
+	res.render('Success', { pageTitle: 'Success' })
+})
+app.get('/students-list', (req, res) => {
+	res.render('List', { pageTitle: 'List' })
+})
+app.post('/add-student', addStudent)
+
+app.use(getNotFoundPage)
+
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
